c12ab3789fceeecf97da679dfdec0fa56535a986
Milad Rk <milad@livedooh.com>
2016-11-22 11:33:39 +0100

    add material design remove unnecessary files

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'devnest-app',
+    templateUrl: 'app/app.component.html',
+    //styleUrls: [ 'app.component.css' ]
+})
+export class AppComponent { }
